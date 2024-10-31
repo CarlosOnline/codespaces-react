f8bbcc149a42272419f19cf637cbb24315a27821
CarlosOnline <cgomes@iinet.com>
2024-10-31 00:01:26 +0000

    Test commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,6 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
           <a
             className="App-link"
             href="https://reactjs.org"
